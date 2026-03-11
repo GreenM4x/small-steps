6d059b736a2df884266a2104271a8460527fa660
ascii <greenm4x@gmail.com>
2026-03-11 22:30:57 +0100

    DAY 3 - Jumping and Moving + Animations

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -7,13 +7,16 @@ extends CharacterBody3D
 @export_group("Movement")
 @export var move_speed := 8.0
 @export var acceleration := 20.0
-
-
+@export var rotation_speed := 12.0
+@export var jump_impulse := 12.0
 
 var _camera_input_direction := Vector2.ZERO
+var _last_movement_direction := Vector3.BACK
+var _gravity := -30.0
 
 @onready var _camera_pivot: Node3D = %CameraPivot
 @onready var _camera: Camera3D = %Camera3D
+@onready var _skin: SophiaSkin = %SophiaSkin
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("left_click"):
@@ -41,17 +44,43 @@ func _physics_process(delta: float) -> void:
 	_camera_pivot.rotation.y -= look_input.x * delta
 
 	_camera_input_direction = Vector2.ZERO
-	
+
 	#Player Movement
 	var raw_input := Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	var forward := _camera.global_basis.z
 	var right := _camera.global_basis.x
-	
+
 	var move_direction := forward * raw_input.y + right * raw_input.x
 	move_direction.y = 0.0
 	move_direction = move_direction.normalized()
-	
+
+	var y_velocity := velocity.y
+	velocity.y = 0.0
 	velocity = velocity.move_toward(move_direction * move_speed, acceleration * delta)
+	velocity.y = y_velocity + _gravity * delta
+
+	var is_starting_jump := Input.is_action_just_pressed("jump") and is_on_floor()
+	if is_starting_jump:
+		velocity.y += jump_impulse
+
 	move_and_slide()
 	
+	if move_direction.length() > 0.2:
+		_last_movement_direction = move_direction
+	var target_angle := Vector3.BACK.signed_angle_to(_last_movement_direction, Vector3.UP)
+	_skin.global_rotation.y = lerp_angle(_skin.rotation.y, target_angle, rotation_speed * delta)
+	
+	if is_starting_jump:
+		_skin.jump()
+	elif not is_on_floor() and velocity.y > 0:
+		_skin.fall()
+	elif is_on_floor():
+		var ground_speed := velocity.length()
+		if ground_speed > 0.0:
+			_skin.move()
+		else :
+			_skin.idle()
+
+
+
 	
